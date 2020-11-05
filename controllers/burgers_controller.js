fbae9a4f22e7a523d965a7f87602788bc23fd883
Paige Burlew <paigeburlew@gmail.com>
2020-11-04 19:22:03 -0600

    controller setup

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,3 +2,41 @@ var express = require("express");
 
 var router = express.Router();
 var burger = require("../models/burger.js");
+
+
+
+router.get("/", function(req, res) {
+    res.redirect("/burgers");
+  });
+  
+  router.get("/burgers", function(req, res) {
+    
+    burger.all(function(burgerData) {
+      
+      res.render("index", { burger_data: burgerData });
+    });
+  });
+  
+  // post route -> back to index
+  router.post("/burgers/create", function(req, res) {
+    
+    burger.create(req.body.burger_name, function(result) {
+     
+      console.log(result);
+      res.redirect("/");
+    });
+  });
+  
+  // put route -> back to index
+  router.put("/burgers/:id", function(req, res) {
+    burger.update(req.params.id, function(result) {
+
+     
+      console.log(result);
+      // Send back response and let page reload from .then in Ajax
+      res.sendStatus(200);
+    });
+  });
+  
+  module.exports = router;
+  
