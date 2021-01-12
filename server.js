2c5d3d5604fed76a2502f09268418da5cd80b6bf
Paige Burlew <paigeburlew@gmail.com>
2021-01-11 18:46:01 -0600

    hw fixed

--- server.js
+++ server.js
@@ -1,4 +1,22 @@
 // Dependencies
 var express = require("express");
 
+const exphbs = require("express-handlebars");
+const routes = require("./controllers/burgers_controllers");
 
+const PORT = process.env.PORT || 8080;
+const app = express();
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+    console.log("Listening on port: " + PORT);
+});
